05ab9d7e78e2cc2dfcf9d96c3c86779108799417
hilltailor <hilltailor@email.arizona.edu>
2022-03-24 17:06:34 -0700

    Update Blood_Quantification.py

--- examples/Blood_Quantification.py
+++ examples/Blood_Quantification.py
@@ -7,11 +7,14 @@ import time
 # Variables
 wavelength1 = 540
 wavelength2 = 680
+h_blood = 
 I_white1 = np.random(720,540)
 I_white2 = np.random(720,540)
 b1_w1 = 50
 b2_w2 = 25
 g1 = b1_w1/b2_w2
+R_lambda_1 = I_white1 * ((h_blood)*b1_w1 + b2_w2)
+R_lambda_2 = I_white2 * ((h_blood)*b1_w1 + b2_w2)
 
 cap = cv2.VideoCapture(0)
 
@@ -22,7 +25,6 @@ while True:
     # dimensions = img.shape
     # print(dimensions)
 
-    
     
 
     if(cv2.waitKey(10) & 0xFF == ord('q')):
