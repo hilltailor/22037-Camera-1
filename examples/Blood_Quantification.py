28da87037d90003ec06dc4feea34cc1575c0c053
hilltailor <hilltailor@email.arizona.edu>
2022-03-24 17:45:56 -0700

    Update Blood_Quantification.py

--- examples/Blood_Quantification.py
+++ examples/Blood_Quantification.py
@@ -5,18 +5,23 @@ import numpy as np
 import time
 
 # Variables
+h_blood_const =
+w_const = 
+m_const =
+f_const =
+mu_a =
+mu_s =
 wavelength1 = 540
 wavelength2 = 680
 h_blood = 
 I_white1 = np.random(720,540)
 I_white2 = np.random(720,540)
 b1_w1 = 50
-b2_w2 = 25
+b2_w2 = 25 # b1_w1 > b2_w2
 g1 = b1_w1/b2_w2
 R_lambda_1 = I_white1 * ((h_blood)*b1_w1 + b2_w2)
 R_lambda_2 = I_white2 * ((h_blood)*b1_w1 + b2_w2)
-Ratio = (I_white1/I_white2) * ((h_blood)*g1 + 1)
-
+R = (I_white1/I_white2) * ((h_blood)*g1 + 1)
 
 cap = cv2.VideoCapture(0)
 
